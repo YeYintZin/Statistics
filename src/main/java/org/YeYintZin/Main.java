69ff092e5c7ccdd30614362b1463bf156b5e5234
Ye Yint Zin <yeyintzin1@gmail.com>
2025-04-24 15:30:45 -0400

    4/24/2025: Second Update, added method mean() and getValue() Fixes to do: Error on (1,1), first row not being titles Find a way to make the table a global variable

--- src/main/java/org/YeYintZin/Main.java
+++ src/main/java/org/YeYintZin/Main.java
@@ -4,15 +4,21 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Double.parseDouble("1"));
-        System.out.println(readData("src/main/resources/data.csv"));
+
     }
 
+    /**
+     * Creates a table from a CSV file
+     * @param path Content Source Root path
+     * @return Table
+     */
     public static List<List<Double>> readData(String path) {
+        //TODO Fix first index first row error
         if (path == null) {
             return null;
         }
@@ -28,6 +34,11 @@ public class Main {
         return table;
     }
 
+    /**
+     * Converts String[] -> List(Double)
+     * @param datas The inputted String
+     * @return datas as a list of double
+     */
     public static List<Double> strToDbl(String[] datas) {
         List<Double> row = new ArrayList<>();
         for (String data : datas) {
@@ -38,5 +49,34 @@ public class Main {
             }
         }
         return row;
+    }
+
+    /**
+     * Calculates the mean of a column of data
+     * @param clm The column, as an index
+     * @param table The table
+     * @return The mean/average of the dataset
+     */
+    public static double mean(int clm, List<List<Double>> table) {
+        List<Double> values = new ArrayList<>();
+        double sum = 0;
+        for (List<Double> row : table) {
+            values.add(row.get(clm));
+        }
+        for (double value : values) {
+            sum += value;
+        }
+        return sum / values.size();
+    }
+
+    /**
+     * Get a value from the table
+     * @param row row
+     * @param clm column
+     * @param table data
+     * @return value at (row,clm)
+     */
+    public static double getValue(int row, int clm, List<List<Double>> table) {
+        return table.get(row).get(clm);
     }
 }
